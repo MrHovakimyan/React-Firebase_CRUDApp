d850bbbbb40bb10bd2e26c1336da1b5f9f2ebe49
Hayk Hovakimyan <hghovakimyan@outlook.com>
2022-07-05 16:35:29 +0400

    feat: create NavBar component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import AddEditUser from "./pages/AddEditUser";
 import Home from "./pages/Home";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/add" element={<AddEditUser />} />
---
+++ src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Menu, Container, Button, Image } from "semantic-ui-react";
+import { useNavigate, Link } from "react-router-dom";
+import logo from "../assets/react-logo.svg";
+
+const NavBar = () => {
+  const navigate = useNavigate();
+  return (
+    <Menu inverted borderless style={{ padding: "0.3rem", marginBottom: "20px" }} attached>
+      <Container>
+        <Menu.Item name="home">
+          <Link to="/">
+            <Image size="min" src={logo} alt="logo" />
+          </Link>
+        </Menu.Item>
+
+        <Menu.Item>
+          <h2>React Firebase CRUD with Image Upload</h2>
+        </Menu.Item>
+
+        <Menu.Item position="right">
+          <Button size="mini" primary onClick={() => navigate("/add")}>
+            Add User
+          </Button>
+        </Menu.Item>
+      </Container>
+    </Menu>
+  );
+};
+
+export default NavBar;
