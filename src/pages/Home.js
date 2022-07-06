2eb068a3892cea94015b67450d78f8017d6543d2
Hayk Hovakimyan <hghovakimyan@outlook.com>
2022-07-06 14:45:38 +0400

    feat: create modal w/user info and with delete user functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { db } from "../firebase";
-import { Button, Card, Grid, Container, Image } from "semantic-ui-react";
-import { collection, onSnapshot } from "firebase/firestore";
+import { Button, Card, Grid, Container, Image, Modal } from "semantic-ui-react";
+import { collection, deleteDoc, onSnapshot, doc } from "firebase/firestore";
+import ModalComp from "../components/ModalComp";
+import { async } from "@firebase/util";
 
 const Home = () => {
   const [users, setUsers] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [user, setUser] = useState({});
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
+  // for rendering added users info
   useEffect(() => {
     setLoading(true);
     const unsub = onSnapshot(
@@ -30,15 +35,32 @@ const Home = () => {
     };
   }, []);
 
+  const handleModal = (item) => {
+    setOpen(true);
+    setUser(item);
+  };
+
+  const handleDelete = async (id) => {
+    if (window.confirm("Ary you sure that you want to delete this user ?")) {
+      try {
+        setOpen(false);
+        await deleteDoc(doc(db, "users", id));
+        setUsers(users.filter((user) => user.id !== id));
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
     <Container>
       <Card.Group>
-        <Grid stackable>
+        <Grid columns={3} stackable>
           {users &&
             users.map((item) => {
               return (
                 <Grid.Column key={item.id}>
-                  <Card key={item.id}>
+                  <Card>
                     <Card.Content>
                       <Image
                         src={item.img}
@@ -54,7 +76,17 @@ const Home = () => {
                         <Button color="green" onClick={() => navigate(`/update/${item.id}`)}>
                           Update
                         </Button>
-                        <Button color="purple">View</Button>
+                        <Button color="purple" onClick={() => handleModal(item)}>
+                          View
+                        </Button>
+                        {open && (
+                          <ModalComp
+                            open={open}
+                            setOpen={setOpen}
+                            handleDelete={handleDelete}
+                            {...user}
+                          />
+                        )}
                       </div>
                     </Card.Content>
                   </Card>
